020889d8eb9fab945b9f9208bf4db244f9ce9c99
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2016-12-05 17:54:23 -0500

    Rule mining working version

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from algorithms.apriori import *
 from Data_Set_gen.Data_prepare import *
 
 #Data_prepare()
-min_sup = 0.01
-min_conf = 0.03
+min_sup = 0.1
+min_conf = 0.003
 apriori(min_sup,min_conf)
